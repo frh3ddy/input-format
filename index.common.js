1bffbddd0400cce71a307f71a547f2e659bc2ebc
Fredy <fr3ddy@me.com>
2017-02-22 18:02:23 -0500

    Update index.common.js

--- index.common.js
+++ index.common.js
@@ -2,11 +2,6 @@
 
 exports = module.exports = {}
 
-exports.InputController = require('./build/input-controller').default
-exports.templateParser = require('./build/template-parser').default
 exports.templateFormatter = require('./build/template-formatter').default
-exports.parseDigit = require('./build/parse-digit').default
-exports.parse = require('./build/parse').default
-exports.format = require('./build/format').default
 
 // exports['default'] = ...
