4d3b157909374933984ef68853d6c15448f127d1
Fredy <fr3ddy@me.com>
2017-02-22 17:57:03 -0500

    Update index.common.js

--- index.common.js
+++ index.common.js
@@ -2,11 +2,10 @@
 
 exports = module.exports = {}
 
-exports.InputController = require('./build/input controller').default
-exports.ReactInput = require('./build/react input').default
-exports.templateParser = require('./build/template parser').default
-exports.templateFormatter = require('./build/template formatter').default
-exports.parseDigit = require('./build/parse digit').default
+exports.InputController = require('./build/input-controller').default
+exports.templateParser = require('./build/template-parser').default
+exports.templateFormatter = require('./build/template-formatter').default
+exports.parseDigit = require('./build/parse-digit').default
 exports.parse = require('./build/parse').default
 exports.format = require('./build/format').default
 
